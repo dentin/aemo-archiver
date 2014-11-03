62edc4f61ce3ebd68f8897cb3ef5fc26edeebeb7
Alex Mason <Alexander.Mason@nicta.com.au>
2014-11-03 12:35:29 +1100

    Refactor the `every` function to more accurately provide periodic activation using getCurrentTime

--- Main.hs
+++ Main.hs
@@ -122,20 +122,31 @@ fetchDaily5mActualLoad = do
 
         -- Insert files into database
         mapM_ (runDB . insertCSV) parsed
+
+-- | Run an event every n time units. Does not guarantee that it will be run
+-- each occurance of n time units if the action takes longer than n time to run.
+-- It will run each action at the next block of n time (it can miss deadlines).
 every :: TimeUnit a => IO () -> a -> IO (ThreadId,MVar ())
 every act t = do
-    now <- getCPUTimeWithUnit :: IO Microsecond
     mv <- newEmptyMVar
-    let ms = convertUnit t :: Microsecond
 
-    tid <- forkIO (run now ms >> putMVar mv ())
+    now <- getCurrentTime
+    let ms = toMicroseconds t
+        ps = ms * 1000000
+        d = toEnum . fromIntegral $ ps :: NominalDiffTime
+        activations = iterate (addUTCTime d) now
+
+    tid <- forkIO (run activations >> putMVar mv ())
     return (tid, mv)
     where
-        run n ms = do
-            let next = addTime n ms :: Microsecond
-            threadDelay (fromIntegral next)
+        run :: [UTCTime] -> IO ()
+        run ts = do
             act
-            run next ms
+            now <- getCurrentTime
+            case dropWhile (<= now) ts of
+                (next:ts') ->
+                    let delay = fromEnum (diffUTCTime next now) `div` 1000000
+                    in threadDelay delay >> run ts'
 
 
 
