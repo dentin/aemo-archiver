28d864af973dab513d0de74f43002dfe548a8ba1
Alex Mason <Alexander.Mason@nicta.com.au>
2015-04-29 12:08:46 +1000

    initDB now loads and inserts all the initialisation CSVs
    TODO: Handle updates to the CSVs more robustly - check for DUIDexists in locs
    	  and whether the power station exists.

--- initDB.hs
+++ initDB.hs
@@ -1,14 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import           Data.ByteString.Lazy        (ByteString)
 import qualified Data.ByteString.Lazy        as B
-import           Data.Vector (Vector)
-import qualified Data.Vector as V
+import           Data.Vector                 (Vector)
+import qualified Data.Vector                 as V
 
-import           Data.Text (Text)
-import qualified Data.Text as T
+import           Data.Text                   (Text)
+import qualified Data.Text                   as T
 
-import Data.Csv (decode, HasHeader(..))
+import           Data.Csv                    (HasHeader (..), Header, decode,
+                                              decodeByName)
 
 
 
@@ -23,13 +26,17 @@ import           Control.Monad.IO.Class      (liftIO)
 
 import           Database.Persist.Postgresql
 
+import           AEMO.CSV
 import           AEMO.Database
 import           AEMO.Types
-import AEMO.CSV
 
 import           Control.Lens
 
-import Control.Monad.Logger (LogLevel(..))
+import           Control.Monad.Logger        (LogLevel (..))
+
+import           Control.Applicative
+import qualified Data.Attoparsec.Text        as A
+import qualified Data.Vector                 as V
 
 
 gensAndLoads :: FilePath
@@ -50,29 +57,56 @@ main = do
 
             migrateDb
 
-            -- Check if we have any power stations, otherwise initialise them
-            -- numStations <- runDB $ count ([] :: [Filter PowerStation])
-            -- when (numStations == 0) $
-            liftIO $ do
-                exists <- doesFileExist stationLocs
-                unless (exists) $ do
-                    putStrLn $ "File does not exist: " ++ stationLocs
-                    exitWith $ ExitFailure 1
-                bs <- B.readFile stationLocs
-                rows <- either error return $ parseGensAndSchedLoads bs
+            numLocs <- runDB $ count ([] :: [Filter DuidLocation])
+            when (numLocs == 0) $ do
+                locs <- liftIO $ do
+                    exists <- doesFileExist stationLocs
+                    unless (exists) $ do
+                        putStrLn $ "File does not exist: " ++ stationLocs
+                        exitWith $ ExitFailure 1
+                    bs <- B.readFile stationLocs
+                    rows <- either error return $ parseGensAndSchedLoads bs
 
-                print $ V.length rows
-                V.mapM_ print rows
-                -- runDB $ do
-                    -- insertMany_ $ map ()
+                    print $ V.length rows
+                    V.mapM_ print rows
+                    return rows
+
+                runDB $ do
+                    insertMany_ . map (\(duid, (lat, lon), comm) -> DuidLocation duid lat lon comm)
+                                . V.toList
+                                $ locs
+            return ()
+
+                        -- Check if we have any power stations, otherwise initialise them
+            numStations <- runDB $ count ([] :: [Filter PowerStation])
+            when (numStations == 0) $ do
+                ps <- liftIO $ do
+                    exists <- doesFileExist gensAndLoads
+                    unless (exists) $ do
+                        putStrLn $ "File does not exist: " ++ gensAndLoads
+                        exitWith $ ExitFailure 1
+                    bs <- B.readFile gensAndLoads
+                    (hrs,rows) <- either error return $ decodeByName bs :: IO ((Header, V.Vector PowerStation))
+                    -- rows <- either error return $ decode HasHeader bs :: IO (V.Vector (V.Vector ByteString))
+                    print $ V.length rows
+                    V.mapM_ print rows
+                    return rows
+
+                runDB . insertMany_ . V.toList $ ps
+            return ()
 
 
             -- Get the names of all known zip files in the database
-            -- knownZipFiles <- allDbZips
+            knownZipFiles <- allDbZips
 
-            -- fetchArchiveActualLoad knownZipFiles
-            -- fetchDaily5mActualLoad knownZipFiles
+            fetchArchiveActualLoad knownZipFiles
+            fetchDaily5mActualLoad knownZipFiles
 
 
-parseGensAndSchedLoads :: ByteString -> Either String (Vector (Text, Text, Text))
-parseGensAndSchedLoads bs =  decode NoHeader bs
+parseGensAndSchedLoads :: ByteString -> Either String (V.Vector (Text, (Double, Double), Text))
+parseGensAndSchedLoads bs =  do
+    vec <- decode NoHeader bs
+    V.mapM prs vec where
+        prs (duid,llstr,comm) = do
+            ll <- A.parseOnly ((,) <$> A.double <*> (" " *> A.double)) llstr
+            return (duid,ll,comm)
