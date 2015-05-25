3a9fe8c33eff29645d038bc3612553ac076eacdf
Alex Mason <Alexander.Mason@nicta.com.au>
2015-05-25 15:08:27 +1000

    Make changes needed to search for configuration in /etc/aremi

--- initDB.hs
+++ initDB.hs
@@ -53,9 +53,9 @@ stationLocs = "power_station_metadata/power_station_locations.csv"
 main :: IO ()
 main = do
     hSetBuffering stdout NoBuffering
-    (conf,_tid) <- C.autoReload C.autoConfig ["service.conf"]
-    connStr <- C.require conf "aemo.db-conn-string"
-    conns <- C.lookupDefault 10 conf "aemo.db-connections"
+    (conf,_tid) <- C.autoReload C.autoConfig ["/etc/aremi/aemo.conf"]
+    connStr <- C.require conf "db-conn-string"
+    conns <- C.lookupDefault 10 conf "db-connections"
 
     execAppM (AS Nothing makeLog LevelInfo) $ do
         withPostgresqlPool connStr conns $ \conn -> do
